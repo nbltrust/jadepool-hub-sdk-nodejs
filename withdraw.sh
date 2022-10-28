494a65eaa2bc64e0978c0d40d05c399a8f4b2e79
xuyang <yang.xu@nbltrust.com>
2022-10-28 23:19:03 +0800

    random value

--- withdraw.sh
+++ withdraw.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-for i in {1..10000}
+for i in {1..1000000}
 do
     echo "----------withdraw $i start---------"
-    timestamp=$(date +%s)
+    timestamp=$(date +%s)$RANDOM
     sleep 1s
-    ./bin/cli.js s-withdraw hsa10 ETH 0xEc67A59e54A393b702c7EcCe1faca731E4f0e601 0.0000000001 $timestamp
+    ./bin/cli.js s-withdraw hsa10 ETH 0xEc67A59e54A393b702c7EcCe1faca731E4f0e601 0.0000001$RANDOM $timestamp
+    sleep 1s
+    timestamp=$(date +%s)$RANDOM
+    ./bin/cli.js s-withdraw hsa10 BTC mxFz9t3FShhrQa7nif3iZLhQW8hxbfZ3N2 0.0000001$RANDOM $timestamp
     echo "----------withdraw $i end-----------"
 done
-
